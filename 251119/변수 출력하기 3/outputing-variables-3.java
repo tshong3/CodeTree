6a9444ba418c9ed6069ec9bee9d0fb6e958ea000
Tae Seong Hong <48014624+tshong3@users.noreply.github.com>
2025-11-19 15:05:56 +0900

    Title: 변수 출력하기 3, Time: 96ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251119/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String a = "1";
+        String b = "2";
+        String c = "C";
+        System.out.print(a+"->"+b+"->"+c);
+    }
+}
